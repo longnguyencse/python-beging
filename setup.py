5b683ff5de74671f28287aa4fc0a85707adce9dd
longnguyencse <longnguyenqu@gmail.com>
2018-12-19 11:52:43 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+setuptools.setup(
+    name="Example numpy",
+    version="0.0.1",
+    author="Example",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+# install_requires=read('requirements.txt').strip().splitlines(),
